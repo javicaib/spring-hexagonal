6c50fe01e30d53e5415095dadb09232b7c39f4de
Javier A. Roque Sañudo <javicaib.work@gmail.com>
2024-09-28 00:33:06 +0200

    Initial Commit

---
+++ src/main/java/cu/javidev/fastdelivery/FastDeliveryApplication.java
@@ -0,0 +1,13 @@
+package cu.javidev.fastdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastDeliveryApplication.class, args);
+    }
+
+}
