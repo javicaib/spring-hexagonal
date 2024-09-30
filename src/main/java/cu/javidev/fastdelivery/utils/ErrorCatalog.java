589d0cf4bb9eaa13cf8da07081b8a42456f0257c
Javier A. Roque Sañudo <javicaib.work@gmail.com>
2024-10-01 01:20:27 +0200

    add deny all security error

--- src/main/java/cu/javidev/fastdelivery/utils/ErrorCatalog.java
+++ src/main/java/cu/javidev/fastdelivery/utils/ErrorCatalog.java
@@ -10,6 +10,7 @@ public enum ErrorCatalog {
     PRODUCT_INVALID("ERROR_PRODUCT_002","Invalid product parameters."),
     GENERIC_ERROR("ERROR_GEN_001","A catastrophic error has occurred."),
     PERSISTENCE_DUPLICATED_ENTRY_ERROR("ERROR_PERSISTENCE_001","A duplicated entry has occurred."),
+    SECURITY_ERROR_DENY("ERROR_SECURITY_001","Request Denied."),
     ;
 
     private final String code;
