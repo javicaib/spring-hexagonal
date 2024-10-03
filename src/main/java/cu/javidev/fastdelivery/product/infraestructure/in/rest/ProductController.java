3c75e9442b30c6ad3265428d5c9419ceed70bd08
javicaib <javicaib.mail@gmail.com>
2024-10-03 03:03:54 +0200

    add PreAuthorize permit all for testing

--- src/main/java/cu/javidev/fastdelivery/product/infraestructure/in/rest/ProductController.java
+++ src/main/java/cu/javidev/fastdelivery/product/infraestructure/in/rest/ProductController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 @RequestMapping("/products")
 @RequiredArgsConstructor
 @Slf4j
+@PreAuthorize("permitAll()")
 public class ProductController {
     private final ProductService service;
     private final ProductRestMapper mapper;
